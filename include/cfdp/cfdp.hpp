7d3353be7168eb4b65ab544de3d7f7589036b3f6
WiktorNowak <wnowak2f@gmail.com>
2024-09-13 14:47:39 +0200

    chore: setup formatting and initial cmake

---
+++ include/cfdp/cfdp.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace cfdp
+{
+void hello_world();
+}
---
+++ src/cfdp.cpp
@@ -0,0 +1,4 @@
+#include <cfdp/cfdp.hpp>
+#include <iostream>
+
+void cfdp::hello_world() { std::cout << "I am alive!" << std::endl; }
