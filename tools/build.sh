338931fd3218b5db27e8f6088debb2ae254c0c50
Qlas <rafgor7@gmail.com>
2024-09-14 14:35:53 +0200

    fix: gcc build

--- tools/build.sh
+++ tools/build.sh
@@ -8,28 +8,28 @@ clean_run="0"
 configure_cmake="0"
 
 while [[ $# -gt 0 ]]; do
-    case $1 in
-        --gcc)
-        cxx_compiler="g++"
-        shift
-        ;;
-        --clang)
-        cxx_compiler="clang++"
-        shift
-        ;;
-        --no-cache)
-        clean_run="1"
-        shift
-        ;;
-        --configure)
-        configure_cmake="1"
-        shift
-        ;;
-        *)
-        echo "Unknown option $1"
-        exit 1
-        ;;
-    esac
+	case $1 in
+	--gcc)
+		cxx_compiler="g++"
+		shift
+		;;
+	--clang)
+		cxx_compiler="clang++"
+		shift
+		;;
+	--no-cache)
+		clean_run="1"
+		shift
+		;;
+	--configure)
+		configure_cmake="1"
+		shift
+		;;
+	*)
+		echo "Unknown option $1"
+		exit 1
+		;;
+	esac
 done
 
 echo "Build configuration:"
@@ -38,23 +38,23 @@ echo "build dir     = ${build_dir}"
 echo "C++ compiler  = ${cxx_compiler}"
 
 if [ "${configure_cmake}" -eq "1" ]; then
-    echo -e "\nRunning fresh cmake configuration...\n"
-    (cd "${repo_root}" && rm -rf build) || true
+	echo -e "\nRunning fresh cmake configuration...\n"
+	(cd "${repo_root}" && rm -rf build) || true
 
-    cmake -G Ninja -D CMAKE_CXX_COMPILER="${cxx_compiler}" -S "${repo_root}" -B build
+	cmake -G Ninja -D CMAKE_CXX_COMPILER="${cxx_compiler}" -S "${repo_root}" -B build
 fi
 
 if [ ! -f "${build_dir}/CMakeCache.txt" ]; then
-    echo -e "\nBuild directory or CMakeCache.txt does not exist."
-    echo "Please run the script with --configure option first."
+	echo -e "\nBuild directory or CMakeCache.txt does not exist."
+	echo "Please run the script with --configure option first."
 
-    exit 1
+	exit 1
 fi
 
 if [ "${clean_run}" -eq "1" ]; then
-    echo -e "\nClearing the cache for fresh build...\n"
+	echo -e "\nClearing the cache for fresh build...\n"
 
-    (cd "${repo_root}" && cmake --build build --target clean)
+	(cd "${repo_root}" && cmake --build build --target clean)
 fi
 
 echo -e "\nBuilding the project...\n"
--- tools/clang_tidy.sh
+++ tools/clang_tidy.sh
@@ -6,8 +6,8 @@ build_root="${repo_root}/build"
 compile_commands="${build_root}/compile_commands.json"
 
 if [ ! -f "${compile_commands}" ]; then
-    echo "Could not find a 'compile_commands.json' file. Please build the project first."
-    exit 1
+	echo "Could not find a 'compile_commands.json' file. Please build the project first."
+	exit 1
 fi
 
 cxx_files_list="$(cd "${repo_root}" && git ls-tree --full-tree --name-only -r HEAD | grep -E ".*(\.cpp|\.hpp)")"
