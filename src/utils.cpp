f9c1e5ecbe3f3a68045e5aace21bb4e05b22d703
Wiktor Nowak <48101734+uncommon-nickname@users.noreply.github.com>
2024-09-20 16:06:00 +0200

    feat: add interface parsing span into BE integer (#11)

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,8 +1,5 @@
 #include "utils.hpp"
 
-#include <exception>
-#include <span>
-
 std::vector<uint8_t> utils::intToBigEndianBytes(uint64_t value, uint8_t size)
 {
     if (size > sizeof(uint64_t))
@@ -11,10 +8,5 @@ std::vector<uint8_t> utils::intToBigEndianBytes(uint64_t value, uint8_t size)
     }
     std::span<uint8_t> view{std::bit_cast<uint8_t*>(&value), size};
 
-#if defined(__APPLE__) || defined(__linux__) || defined(_WIN32)
-    std::vector<uint8_t> bytes{view.rbegin(), view.rend()};
-#else
-    std::vector<uint8_t> bytes{view.begin(), view.end()};
-#endif
-    return bytes;
+    return std::vector<uint8_t>{view.rbegin(), view.rend()};
 };
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <concepts>
 #include <cstdint>
+#include <exception>
 #include <iterator>
+#include <span>
 #include <type_traits>
 #include <vector>
 
@@ -10,15 +13,43 @@ namespace utils
 std::vector<uint8_t> intToBigEndianBytes(uint64_t value, uint8_t size);
 
 template <class T>
-inline void concatenateVectorsInplace(std::vector<T>& src, std::vector<T>& dst)
+    requires std::unsigned_integral<T>
+T bigEndianBytesToInt(const std::span<uint8_t> memoryView);
+
+template <class T>
+inline void concatenateVectorsInplace(std::vector<T>& src, std::vector<T>& dst);
+
+template <class T>
+    requires std::is_enum_v<T>
+inline decltype(auto) toUnderlying(T e);
+} // namespace utils
+
+template <class T>
+    requires std::unsigned_integral<T>
+T utils::bigEndianBytesToInt(const std::span<uint8_t> memoryView)
+{
+    if (memoryView.size_bytes() > sizeof(T))
+    {
+        throw std::invalid_argument{"Passed span contains more bytes than type T can fit"};
+    }
+
+    T result{};
+    for (const auto& it : memoryView)
+    {
+        result = (result << 8) + it;
+    }
+    return result;
+}
+
+template <class T>
+inline void utils::concatenateVectorsInplace(std::vector<T>& src, std::vector<T>& dst)
 {
     dst.insert(dst.end(), std::make_move_iterator(src.begin()), std::make_move_iterator(src.end()));
 }
 
-template <class EnumType>
-    requires std::is_enum_v<EnumType>
-decltype(auto) toUnderlying(EnumType e)
+template <class T>
+    requires std::is_enum_v<T>
+inline decltype(auto) utils::toUnderlying(T e)
 {
-    return static_cast<std::underlying_type_t<EnumType>>(e);
+    return static_cast<std::underlying_type_t<T>>(e);
 }
-} // namespace utils
