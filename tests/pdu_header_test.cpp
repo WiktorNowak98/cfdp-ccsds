2f87ee8d59ff5a923b871e4057d53002aa3c96b3
WiktorNowak <wnowak2f@gmail.com>
2024-09-24 07:52:03 +0200

    tests: compile test executable with gmock

--- tests/pdu_header_test.cpp
+++ tests/pdu_header_test.cpp
@@ -1,9 +1,29 @@
-#include <cfdp/pdu_header.hpp>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-namespace cfdp::pdu::header
+#include <array>
+#include <vector>
+
+#include <cfdp/pdu_enums.hpp>
+#include <cfdp/pdu_header.hpp>
+
+using ::testing::ElementsAreArray;
+
+using ::cfdp::pdu::header::CrcFlag;
+using ::cfdp::pdu::header::Direction;
+using ::cfdp::pdu::header::LargeFileFlag;
+using ::cfdp::pdu::header::PduHeader;
+using ::cfdp::pdu::header::PduType;
+using ::cfdp::pdu::header::SegmentationControl;
+using ::cfdp::pdu::header::SegmentMetadataFlag;
+using ::cfdp::pdu::header::TransmissionMode;
+
+namespace
 {
-TEST(PduHeader, Basic)
+constexpr std::array<uint8_t, 7> encoded_header_frame{51, 0, 5, 0, 1, 1, 2};
+}
+
+TEST(PduHeader, TestHeaderEncoding)
 {
     auto header =
         PduHeader(1, PduType::FileData, Direction::TowardsReceiver, TransmissionMode::Acknowledged,
@@ -12,5 +32,16 @@ TEST(PduHeader, Basic)
                   1, 1, 1, 2);
 
     auto encoded = header.encodeToBytes();
+
+    ASSERT_EQ(encoded.size(), 4 + 3);
+    EXPECT_THAT(encoded, ElementsAreArray(encoded_header_frame));
 }
-} // namespace cfdp::pdu::header
+
+TEST(PduHeader, TestHeaderDecoding)
+{
+    auto encodedHeaderView =
+        std::span<uint8_t const>{encoded_header_frame.begin(), encoded_header_frame.end()};
+    auto header = PduHeader(encodedHeaderView);
+
+    ASSERT_EQ(header.getRawSize(), 7);
+}
