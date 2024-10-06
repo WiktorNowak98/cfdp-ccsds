43724b9cc3354d5402c249ab660dbcb60cd51239
WiktorNowak <wnowak2f@gmail.com>
2024-10-06 22:59:07 +0200

    tests: common lambdas

--- tests/cfdp_runtime/atomic_queue.test.cpp
+++ tests/cfdp_runtime/atomic_queue.test.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 
 using ::cfdp::runtime::atomic::AtomicQueue;
+
 using ::testing::UnorderedElementsAreArray;
 
 class AtomicQueueTest : public testing::Test
@@ -18,13 +19,13 @@ class AtomicQueueTest : public testing::Test
     template <class T>
     std::vector<T> drainQueue(AtomicQueue<T>& queue, size_t expected_size)
     {
-        auto vec = std::vector<T>{};
-        vec.reserve(expected_size);
+        auto vec = std::vector<T>(expected_size);
 
-        for (auto i = 0; i < expected_size; ++i)
+        for (auto& item : vec)
         {
-            vec.push_back(queue.pop());
+            item = queue.pop();
         }
+
         return vec;
     }
 };
@@ -33,19 +34,14 @@ TEST_F(AtomicQueueTest, NoItemDroppedWhenMultipleProducers)
 {
     auto queue = AtomicQueue<uint8_t>{};
 
-    std::array<std::thread, 3> workers = {
-        std::thread{[&]() {
-            for (const auto& i : std::views::iota(0, 5))
-                queue.push(i);
-        }},
-        std::thread{[&]() {
-            for (const auto& i : std::views::iota(5, 10))
-                queue.push(i);
-        }},
-        std::thread{[&]() {
-            for (const auto& i : std::views::iota(10, 15))
-                queue.push(i);
-        }},
+    auto pusher = [&](size_t lb, size_t ub) {
+        for (const auto& i : std::views::iota(lb, ub))
+            queue.push(i);
+    };
+    auto workers = std::array<std::thread, 3>{
+        std::thread{pusher,  0,  5},
+        std::thread{pusher,  5, 10},
+        std::thread{pusher, 10, 15},
     };
 
     for (auto& thread : workers)
@@ -53,9 +49,9 @@ TEST_F(AtomicQueueTest, NoItemDroppedWhenMultipleProducers)
 
     ASSERT_EQ(queue.sizeNow(), 15);
 
-    auto items    = drainQueue(queue, 15);
-    auto expected = std::vector<uint8_t>(15);
+    auto items = drainQueue(queue, 15);
 
+    auto expected = std::vector<uint8_t>(15);
     std::iota(expected.begin(), expected.end(), 0);
 
     EXPECT_THAT(items, UnorderedElementsAreArray(expected));
@@ -65,17 +61,14 @@ TEST_F(AtomicQueueTest, MultipleProducersSingleConsumer)
 {
     auto queue = AtomicQueue<uint8_t>{};
 
-    std::array<std::thread, 2> workers = {
-        std::thread{[&]() {
-            for (const auto& i : std::views::iota(0, 10))
-                queue.push(i);
-        }},
-        std::thread{[&]() {
-            for (const auto& i : std::views::iota(10, 20))
-                queue.push(i);
-        }},
+    auto pusher = [&](size_t lb, size_t ub) {
+        for (const auto& i : std::views::iota(lb, ub))
+            queue.push(i);
     };
-
+    auto workers = std::array<std::thread, 2>{
+        std::thread{pusher,  0, 10},
+        std::thread{pusher, 10, 20},
+    };
     auto items = drainQueue(queue, 20);
 
     for (auto& thread : workers)
