3389d288aa1c8f21b9ec45de2dd0afc22542f319
WiktorNowak <wnowak2f@gmail.com>
2024-10-19 13:52:10 +0200

    refactor: preprocessor magic

--- include/runtime/cfdp_runtime/logger.hpp
+++ include/runtime/cfdp_runtime/logger.hpp
@@ -6,6 +6,26 @@
 #include <mutex>
 #include <thread>
 
+#if defined(CFDP_LOG_LEVEL_TRACE)
+#define LOG_TRACE
+#endif
+
+#if defined(LOG_TRACE) || defined(CFDP_LOG_LEVEL_DEBUG)
+#define LOG_DEBUG
+#endif
+
+#if defined(LOG_DEBUG) || defined(CFDP_LOG_LEVEL_INFO)
+#define LOG_INFO
+#endif
+
+#if defined(LOG_INFO) || defined(CFDP_LOG_LEVEL_WARN)
+#define LOG_WARN
+#endif
+
+#if defined(LOG_WARN) || defined(CFDP_LOG_LEVEL_ERROR)
+#define LOG_ERROR
+#endif
+
 namespace cfdp::runtime::logging
 {
 enum class LogLevel : uint8_t
@@ -16,20 +36,6 @@ enum class LogLevel : uint8_t
     Warn,
     Error,
 };
-
-#if defined(CFDP_LOG_LEVEL_TRACE)
-constexpr LogLevel log_level_cutoff = LogLevel::Trace;
-#elif defined(CFDP_LOG_LEVEL_DEBUG)
-constexpr LogLevel log_level_cutoff = LogLevel::Debug;
-#elif defined(CFDP_LOG_LEVEL_INFO)
-constexpr LogLevel log_level_cutoff = LogLevel::Info;
-#elif defined(CFDP_LOG_LEVEL_WARN)
-constexpr LogLevel log_level_cutoff = LogLevel::Warn;
-#elif defined(CFDP_LOG_LEVEL_ERROR)
-constexpr LogLevel log_level_cutoff = LogLevel::Error;
-#else
-constexpr LogLevel log_level_cutoff = LogLevel::Error;
-#endif
 
 class Logger
 {
@@ -56,45 +62,51 @@ class Logger
 };
 
 template <class... Args>
-static void log(LogLevel level, std::format_string<Args...> msg, Args&&... args) noexcept
+constexpr static void log(LogLevel level, std::format_string<Args...> msg, Args&&... args) noexcept
 {
-    if (level < log_level_cutoff)
-    {
-        return;
-    }
-    auto log = std::vformat(msg.get(), std::make_format_args(args...));
-
     static auto logger = Logger{};
-    logger.log(level, log);
+
+    auto log = std::vformat(msg.get(), std::make_format_args(args...));
+    logger.log(level, std::move(log));
 }
 
 template <class... Args>
-static void trace(std::format_string<Args...> msg, Args&&... args) noexcept
+constexpr static void trace(std::format_string<Args...> msg, Args&&... args) noexcept
 {
+#if defined(LOG_TRACE)
     log(LogLevel::Trace, msg, std::forward<Args>(args)...);
+#endif
 }
 
 template <class... Args>
-static void debug(std::format_string<Args...> msg, Args&&... args) noexcept
+constexpr static void debug(std::format_string<Args...> msg, Args&&... args) noexcept
 {
+#if defined(LOG_DEBUG)
     log(LogLevel::Debug, msg, std::forward<Args>(args)...);
+#endif
 }
 
 template <class... Args>
-static void info(std::format_string<Args...> msg, Args&&... args) noexcept
+constexpr static void info(std::format_string<Args...> msg, Args&&... args) noexcept
 {
+#if defined(LOG_INFO)
     log(LogLevel::Info, msg, std::forward<Args>(args)...);
+#endif
 }
 
 template <class... Args>
-static void warn(std::format_string<Args...> msg, Args&&... args) noexcept
+constexpr static void warn(std::format_string<Args...> msg, Args&&... args) noexcept
 {
+#if defined(LOG_WARN)
     log(LogLevel::Warn, msg, std::forward<Args>(args)...);
+#endif
 }
 
 template <class... Args>
-static void error(std::format_string<Args...> msg, Args&&... args) noexcept
+constexpr static void error(std::format_string<Args...> msg, Args&&... args) noexcept
 {
+#if defined(LOG_ERROR)
     log(LogLevel::Error, msg, std::forward<Args>(args)...);
+#endif
 }
 } // namespace cfdp::runtime::logging
